69e47c43b4902d44bab9cdfa85feca8a5633155f
Steve Green <stevenpkg@au1.ibm.com>
2018-03-13 10:18:07 +1100

    first commit

---
+++ public/js/demo.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* global someTemplate: true, _:true */
+'use strict';
+
+$(document).ready(function() {
+  var template = someTemplate.innerHTML;
+  $('.underscore-example').append(_.template(template, {
+    items: [{
+      name: 'John',
+      age: 27
+    }, {
+      name: 'Jill',
+      age: 29
+    }, {
+      name: 'Someone',
+      age: 100
+    }]
+  }));
+});
+
+/*
+Tabbed Panels js
+*/
+(function() {
+  $('.tab-panels--tab').click(function(e) {
+    e.preventDefault();
+    var self = $(this);
+    var inputGroup = self.closest('.tab-panels');
+    var idName = null;
+
+    inputGroup.find('.active').removeClass('active');
+    self.addClass('active');
+    idName = self.attr('href');
+    $(idName).addClass('active');
+  });
+})();
